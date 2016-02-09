1bc7c06e93417b5a44526c752ec5724927903635
Caleb Nelton <caleb.nelton@gmail.com>
2016-02-09 16:58:34 -0600

    Initial Shell

---
+++ src/SchemaDesigner/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+using SchemaDesigner.Business;
+using System.Collections.Generic;
+
+namespace SchemaDesigner
+{
+    public class ShellViewModel : BindableBase
+    {
+
+        private List<Entity> entities = new List<Entity>();
+        public List<Entity> Entities
+        {
+            get { return this.entities; }
+            set { this.SetProperty<List<Entity>>(ref this.entities, value); }
+
+        }
+    }
+}
